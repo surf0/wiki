21659e41733343251f48dabc09f3d5e8ac882cf5
Joscha von Andrian <joschavonandrian@gmail.com>
2022-08-30 01:26:39 +0200

    fix logo theme unmounted

--- components/logo.js
+++ components/logo.js
@@ -1,11 +1,24 @@
-import { useTheme } from 'next-themes'
+import { useTheme } from 'next-themes';
 
+import { useEffect, useState } from 'react'
+
+export function useMounted() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return mounted
+}
 
 const Logo = (props) => {
     const { theme, setTheme, systemTheme } = useTheme()
-  console.log('theme', theme, systemTheme)
+    const renderedTheme = theme === 'system' ? systemTheme : theme
+    const mounted = useMounted()
+
     return (<img
-    src={theme == 'dark' ? '/logo.png' : '/logo2.png'}
+    src={mounted && renderedTheme === 'dark' ? '/logo.png' : '/logo2.png'}
     alt="logo surf0"
     width={props.width}
   />)
