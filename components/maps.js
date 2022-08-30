060dd839aa5445dc12117153735646ca1d99c72f
Joscha von Andrian <joschavonandrian@gmail.com>
2022-08-31 00:05:57 +0200

    remove paper

--- components/maps.js
+++ components/maps.js
@@ -9,13 +9,6 @@ import { DataGrid, GridToolbar } from '@mui/x-data-grid'
 
 import maps from '../data/maps'
 
-function secondsToDHM(seconds) {
-  var d = Math.floor(seconds / (3600 * 24))
-  var h = Math.floor((seconds % (3600 * 24)) / 3600)
-  var m = Math.floor((seconds % 3600) / 60)
-  return (d !== 0 ? d + 'd ' : '') + (h !== 0 ? h + 'h ' : '') + m + 'm'
-}
-
 export function useMounted() {
   const [mounted, setMounted] = useState(false)
 
@@ -61,22 +54,20 @@ const Maps = () => {
       {mounted && (
         <ThemeProvider theme={muitheme}>
           <Box sx={{ maxWidth: 'xl' }}>
-            <Paper>
-              <Box sx={{ height: 800, width: 1 }}>
-                <DataGrid
-                  rows={rows}
-                  columns={columns}
-                  components={{ Toolbar: GridToolbar }}
-                  getRowId={(row) => row.name}
-                  componentsProps={{
-                    toolbar: {
-                      showQuickFilter: true,
-                      quickFilterProps: { debounceMs: 500 },
-                    },
-                  }}
-                />
-              </Box>
-            </Paper>
+            <Box sx={{ height: 800, width: 1 }}>
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                components={{ Toolbar: GridToolbar }}
+                getRowId={(row) => row.name}
+                componentsProps={{
+                  toolbar: {
+                    showQuickFilter: true,
+                    quickFilterProps: { debounceMs: 500 },
+                  },
+                }}
+              />
+            </Box>
           </Box>
         </ThemeProvider>
       )}
